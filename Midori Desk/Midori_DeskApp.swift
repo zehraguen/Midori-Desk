2fb83326ad02d555291dc75034a73edd120c7f3b
Zehra Gün <90135499+zehraguen@users.noreply.github.com>
2025-07-27 14:44:17 +0300

    Initial Commit

---
+++ Midori Desk/Midori_DeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midori_DeskApp.swift
+//  Midori Desk
+//
+//  Created by Zehra Gün on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Midori_DeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Midori DeskTests/Midori_DeskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Midori_DeskTests.swift
+//  Midori DeskTests
+//
+//  Created by Zehra Gün on 27.07.2025.
+//
+
+import Testing
+@testable import Midori_Desk
+
+struct Midori_DeskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
